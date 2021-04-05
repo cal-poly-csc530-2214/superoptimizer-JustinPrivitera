51cdfb6f7445b2cd6221b4c7d2ff7500ad4e9c05
Justin <justinprivitera@gmail.com>
2021-04-05 13:18:40 -0700

    binops are pretty fleshed out now

--- check.rkt
+++ check.rkt
@@ -10,6 +10,7 @@
 (define nums (cast (range lower-bound upper-bound) (Listof Any)))
 (define arith-symbols (list '+ '*))
 (define logic-symbols (list 'and 'or))
+(define arith-to-logic-symbols (list '<= '< '== '> '>=))
 (define bools (list #t #f))
 
 (define (arith-binops) : (Listof (Listof Any))
@@ -17,6 +18,9 @@
 
 (define (logic-binops) : (Listof (Listof Any))
   (cartesian-product logic-symbols bools bools))
+
+(define (arith-to-logic-binops) : (Listof (Listof Any))
+  (cartesian-product arith-to-logic-symbols nums nums))
 
 (define progs
   (append
@@ -29,7 +33,14 @@
    (logic-binops)
    (cartesian-product logic-symbols (logic-binops) bools)
    (cartesian-product logic-symbols bools (logic-binops))
-   (cartesian-product logic-symbols (logic-binops) (logic-binops))))
+   (cartesian-product logic-symbols (logic-binops) (logic-binops))
+   (arith-to-logic-binops)
+   (cartesian-product arith-to-logic-symbols (arith-binops) nums)
+   (cartesian-product arith-to-logic-symbols nums (arith-binops))
+   (cartesian-product arith-to-logic-symbols (arith-binops) (arith-binops))
+   (cartesian-product logic-symbols (arith-to-logic-binops) bools)
+   (cartesian-product logic-symbols bools (arith-to-logic-binops))
+   (cartesian-product logic-symbols (arith-to-logic-binops) (arith-to-logic-binops))))
 
 (filter (lambda ([s : Any]) : Boolean
           (works? (cast s Sexp) #t)) progs)
